708f927f953d4790420c0c0120c9740fb78df266
Cristian <canteloterrazas02@gmail.com>
2025-12-10 20:22:42 -0400

    amsdclkasmdlckm

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from contextlib import asynccontextmanager
+from typing import Literal
 
 import joblib
 import pandas as pd
-
-# import numpy as np
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 
 
 class CreditLeadInput(BaseModel):
     age: int = Field(description="Edad del cliente", examples=[45])
-    gender: str = Field(description="Género: 'Male' o 'Female'", examples=["Male"])
-    occupation: str = Field(
-        description="Ocupación: 'Salaried', 'Self_Employed', 'Entrepreneur', 'Other'",
+    gender: Literal["Male", "Female"] = Field(
+        description="Género: 'Male' o 'Female'", examples=["Male"]
+    )
+    occupation: Literal["Salaried", "Self_Employed", "Other"] = Field(
+        description="Ocupación: 'Salaried', 'Self_Employed', 'Other'",
         examples=["Salaried"],
     )
     vintage: int = Field(description="Antigüedad en meses", examples=[24])
-    credit_product: str = Field(
+    credit_product: Literal["Yes", "No", "Unknown"] = Field(
         description="Tiene producto de crédito: 'Yes', 'No', 'Unknown'",
         examples=["No"],
     )
     avg_account_balance: float = Field(
         description="Balance promedio en cuenta", examples=[150000.0]
     )
-    is_active: str = Field(description="Cliente activo: 'Yes' o 'No'", examples=["Yes"])
+    is_active: Literal["Yes", "No"] = Field(
+        description="Cliente activo: 'Yes' o 'No'", examples=["Yes"]
+    )
+
+
+class Prediction(BaseModel):
+    label: str
+    probability: float
 
 
 # Objetos importados (exportados desde el notebook)
@@ -49,72 +57,50 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Credit Card Lead Prediction API", lifespan=lifespan)
 
 
-def preprocess_input(data: CreditLeadInput, feature_columns, scaler):
-    """
-    Transforma el JSON de entrada en un DataFrame con el formato exacto
-    que espera el modelo (Scaling + OneHotEncoding).
-    """
-    # Crear DataFrame base
-    df = pd.DataFrame([data.model_dump()])
-
-    # Mapeos Manuales
-    df["Gender"] = df["gender"].map({"Male": 1, "Female": 0})
-    df["Is_Active"] = df["is_active"].map({"Yes": 1, "No": 0})
-    df["Credit_Product"] = df["credit_product"].apply(lambda x: 1 if x == "Yes" else 0)
-
-    # One-Hot Encoding (Occupation) Manual
-    occupation_val = f"Occupation_{data.occupation}"
-
-    # Inicializamos todas las columnas de One-Hot en 0
-    for col in feature_columns:
-        if col.startswith("Occupation_"):
-            df[col] = 0
-
-    # Si la ocupación actual existe en las columnas del modelo, ponemos un 1
-    if occupation_val in feature_columns:
-        df[occupation_val] = 1
-
-    # Renombrar columnas para que coincidan con el scaler (Mayúsculas vs minúsculas del input)
-    df = df.rename(
-        columns={
-            "age": "Age",
-            "vintage": "Vintage",
-            "avg_account_balance": "Avg_Account_Balance",
-        }
+def preprocess_input(data: CreditLeadInput, scaler) -> pd.DataFrame:
+    df = pd.DataFrame(
+        [
+            {
+                "Gender": int(data.gender == "Male"),
+                "Age": data.age,
+                "Vintage": data.vintage,
+                "Credit_Product": int(data.credit_product == "Yes"),
+                "Avg_Account_Balance": data.avg_account_balance,
+                "Is_Active": int(data.is_active == "Yes"),
+                "Occupation_Other": int(data.occupation == "Other"),
+                "Occupation_Salaried": int(data.occupation == "Salaried"),
+                "Occupation_Self_Employed": int(data.occupation == "Self_Employed"),
+            }
+        ]
     )
 
     # Aplicar el scaler cargado
     df = scaler.transform(df)
 
-    # Reordenar columnas para que coincidan exactamente con el entrenamiento
-    df = df.reindex(columns=feature_columns, fill_value=0)
+    # # Reordenar columnas para que coincidan exactamente con el entrenamiento
+    # df = df.reindex(columns=feature_columns, fill_value=0)
 
     return df
 
 
 @app.post("/predict")
-async def predict_lead(lead: CreditLeadInput):
+async def predict_lead(lead: CreditLeadInput) -> Prediction:
     if not ml_models["model"]:
         raise HTTPException(status_code=500, detail="El modelo no está cargado.")
 
     try:
         model = ml_models["model"]
         scaler = ml_models["scaler"]
-        columns = ml_models["columns"]
+        # columns = ml_models["columns"]
 
-        processed_data = preprocess_input(lead, columns, scaler)
+        processed_data = preprocess_input(lead, scaler)
 
         prediction = model.predict(processed_data)[0]
         probability = model.predict_proba(processed_data)[0][1]
 
-        result = "Interesado" if prediction == 1 else "No Interesado"
+        label = "Interesado" if prediction == 1 else "No Interesado"
 
-        return {
-            "prediction": int(prediction),
-            "label": result,
-            "probability_score": round(float(probability), 4),
-            "input_summary": {"age": lead.age, "occupation": lead.occupation},
-        }
+        return Prediction(label=label, probability=round(float(probability), 4))
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error en la predicción: {str(e)}")
 
